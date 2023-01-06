84ce4ac4c8d49eca49695305f1a06e6006e409ab
Alex Hey <64182587+alexhey1999@users.noreply.github.com>
2023-01-06 05:01:44 +0000

    Actually got some things working

--- app/Main.hs
+++ app/Main.hs
@@ -1,39 +1,111 @@
-module Main (main) where
-
+import Control.Concurrent
+import System.IO
 import Users
 import Messages
-import System.IO
 import System.Random
+import Data.UUID
+import Data.UnixTime
 
+-- Random Functions
+-- ---------------------
+
+-- Calculates a random number between 0 and an inputted number
 random_val_in_range :: Int -> IO Int
 random_val_in_range max_val = getStdRandom (randomR (0,max_val))
 
-random_string :: IO String
-random_string = 
+-- Generates a random string of characters of given length
+random_message :: Int -> IO String
+random_message string_length = do
+    gen <- newStdGen
+    let random_message = take string_length $ randomRs ('a','z') gen
+    return random_message
 
-message_list :: [Message]
-message_list = []
+-- UUID Generator modified from https://stackoverflow.com/questions/58906666/haskell-uuid-generation
+gen_uuid :: UUID
+gen_uuid = 
+    let seed = 1
+        seeded_gen = mkStdGen seed
+        (uuid_generated, _) = random seeded_gen
+    in uuid_generated
 
+
+-- Message Thread - Runs 10 times per user defined
+-- Takes channel to denote when all user messages are completed
+-- takes channel to store message created
+-- takes user to show who sent the message
+-- takes a list of remaining users who can be sent the randomised message
+message_thread :: Chan () -> Chan Message -> User -> [User] -> IO ()
+message_thread messages_complete messages_chan s_user u_list = do
+    -- Sleep random time
+    delay <- random_val_in_range 10000000
+    threadDelay delay
+    -- Pick random user from list
+    user_index <- random_val_in_range (length u_list)
+    let selected_user = u_list !! user_index
+    -- Generate string
+    message_content_generated <- random_message 100
+    -- Generate UUID for the message
+    let m_id = gen_uuid
+    
+    -- Get unix timestamp
+    time <- getUnixTime >>= return . show . utSeconds
+    let time_int = read time
+    -- Create Message
+    let message_created =  create_message m_id message_content_generated time_int selected_user s_user
+    
+    putStrLn ("Message Written")
+    writeChan messages_chan message_created
+    writeChan messages_complete ()
+
+user_thread :: Chan () -> Chan Message -> User -> IO ()
+user_thread completed_chan messages_chan user = do
+    putStrLn ("Starting User" ++ (show (user_id user)) ++ " Thread")
+    let possible_users = filter (\a -> a/=user) full_user_list
+    -- putStrLn ("Sendable users: " ++ show possible_users)
+    all_messages_sent <- newChan
+
+    forkIO $ ( message_thread all_messages_sent messages_chan user possible_users)
+    forkIO $ ( message_thread all_messages_sent messages_chan user possible_users)
+    forkIO $ ( message_thread all_messages_sent messages_chan user possible_users)
+    forkIO $ ( message_thread all_messages_sent messages_chan user possible_users)
+    forkIO $ ( message_thread all_messages_sent messages_chan user possible_users)
+    forkIO $ ( message_thread all_messages_sent messages_chan user possible_users)
+    forkIO $ ( message_thread all_messages_sent messages_chan user possible_users)
+    forkIO $ ( message_thread all_messages_sent messages_chan user possible_users)
+    forkIO $ ( message_thread all_messages_sent messages_chan user possible_users)
+    forkIO $ ( message_thread all_messages_sent messages_chan user possible_users)
+
+    mapM_ (\_ -> readChan all_messages_sent) [1..10]
+
+    writeChan completed_chan ()
+
+main :: IO ()
 main = do
-    hSetBuffering stdout NoBuffering
-    let test_user = User {user_id = 0, username = "Test Username"}
-    simulate_thread test_user
+    threads_completed <- newChan
+    messages_chan <- newChan
+
+    forkIO $ (user_thread threads_completed messages_chan (create_user 1 "Person 1"))
+    forkIO $ (user_thread threads_completed messages_chan (create_user 2 "Person 2"))
+    forkIO $ (user_thread threads_completed messages_chan (create_user 3 "Person 3"))
+    forkIO $ (user_thread threads_completed messages_chan (create_user 4 "Person 4"))
+    forkIO $ (user_thread threads_completed messages_chan (create_user 5 "Person 5"))
+    forkIO $ (user_thread threads_completed messages_chan (create_user 6 "Person 6"))
+    forkIO $ (user_thread threads_completed messages_chan (create_user 7 "Person 7"))
+    forkIO $ (user_thread threads_completed messages_chan (create_user 8 "Person 8"))
+    forkIO $ (user_thread threads_completed messages_chan (create_user 9 "Person 9"))
+    forkIO $ (user_thread threads_completed messages_chan (create_user 10 "Person 10"))
+    
+    mapM_ (\_ -> readChan threads_completed) [1..10]
+
+    print "Threads Complete"
+
+    -- mapM_ (\message -> readChan messages_chan) [1..100]
 
 
-    -- map (create_thread message_list) user_list
-    -- create_thread message_list test_user
-    -- mapM_ print user_list
+    return ()
 
-create_user :: Int -> String -> User
-create_user id_parse username_parse = User {user_id = id_parse, username = username_parse}
-
-
-create_message :: Int -> String -> String -> User -> User -> Message
-create_message message_id_parse message_content_parse time_created_parse user_sent_to_parse user_sent_from_parse = Message {message_id = message_id_parse, message_content = message_content_parse, time_created = time_created_parse, user_sent_to = user_sent_to_parse, user_sent_from_parse = user_sent_from}
-
-
-user_list :: [User]
-user_list = [
+full_user_list :: [User]
+full_user_list = [
         create_user 1 "Person 1",
         create_user 2 "Person 2",
         create_user 3 "Person 3",
@@ -46,11 +118,8 @@ user_list = [
         create_user 10 "Person 10"
     ]
 
-simulate_thread :: User -> IO ()
-simulate_thread sending_user = do
-    user_index <- random_val_in_range (length user_list -1)
-    let random_user = user_list !! user_index
+create_user :: Int -> String -> User
+create_user id_parse username_parse = User {user_id = id_parse, username = username_parse}
 
-    create_message 
-    
-
+create_message :: UUID -> String -> Int -> User -> User -> Message
+create_message message_id_parse message_content_parse time_created_parse user_sent_to_parse user_sent_from_parse = Message {message_id = message_id_parse, message_content = message_content_parse, time_created = time_created_parse, user_sent_to = user_sent_to_parse, user_sent_from = user_sent_from_parse}
